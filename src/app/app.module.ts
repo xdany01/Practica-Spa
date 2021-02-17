1abf45381bbba1842d291c4959ba004fa2baa00c
Daniel <xd_dany@me.com>
2021-02-17 19:34:53 -0300

    agregue un slice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
